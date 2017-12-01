2b5c7eb30255276c71e67983a17b41c06aa2d37a
jianghang <hangscer@gmail.com>
2017-12-01 20:44:34 +0800

    all

--- centerRouter/src/main/scala/nathan/Main.scala
+++ centerRouter/src/main/scala/nathan/Main.scala
@@ -1,10 +1,14 @@
 package nathan
 
+import nathan.dbentity.EntityTable._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import nathan.ec.ExecutorService
 import nathan.router.IndexRouter
 import slick.jdbc.H2Profile.api._
+import scala.concurrent.duration._
+import scala.concurrent.Await
+
 object Main extends App {
   implicit val system = ExecutorService.system
   implicit val mat = ExecutorService.mat
@@ -12,12 +16,11 @@ object Main extends App {
 
   def route: Route = {
     val indexRouter = new IndexRouter()
-     indexRouter.route
+    indexRouter.route
   }
 
   Http().bindAndHandle(route, "127.0.0.1", 8888)
-  val db=Database.forConfig("h2local")
-  println(db)
-  Thread.sleep(2000)
-  println(db.source.maxConnections)
+  val db = Database.forConfig("h2local")
+  val r=Await.result(db.run(users.schema.create.asTry), 10 seconds) //重复创建 已经存在的话，就出错
+  
 }
--- centerRouter/src/main/scala/nathan/dbentity/EntityTable.scala
+++ centerRouter/src/main/scala/nathan/dbentity/EntityTable.scala
@@ -1,16 +1,26 @@
 package nathan.dbentity
+
 import slick.jdbc.H2Profile.api._
+
 object EntityTable {
 
   case class UserEntity(id: Long, username: String, password: String, `type`: String, lastLoginTime: Long)
 
   class User(_tableTag: Tag) extends Table[UserEntity](_tableTag, "user") {
-    def * = (id, username, password, `type`, lastLoginTime)
-    def id: Rep[Long]=
-    def username: String
-    def password: String
-    def `type`: String
-    def lastLoginTime: Long
+    def * = (id, username, password, `type`, lastLoginTime) <> (UserEntity.tupled, UserEntity.unapply)
+
+    val id: Rep[Long] = column[Long]("id")
+
+    val username: Rep[String] = column[String]("username", O.Unique)
+
+    val password: Rep[String] = column[String]("password")
+
+    val `type`: Rep[String] = column[String]("type")
+
+    val lastLoginTime: Rep[Long] = column[Long]("lastLoginTime")
+
+    val pk = primaryKey("id_pk", (id))
   }
 
+  val users = new TableQuery(tag => new User(tag))
 }
