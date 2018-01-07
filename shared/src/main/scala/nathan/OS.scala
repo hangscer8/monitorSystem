9f115233345d99137acc53134fd638c604e7dc25
jianghang <hangscer@hotmail.com>
2018-01-07 16:28:01 +0800

    获取`jps -l`，并解析数据

--- shared/src/main/scala/nathan/OS.scala
+++ shared/src/main/scala/nathan/OS.scala
@@ -1,6 +1,6 @@
 package nathan
 
-import nathan.Protocols.top.mac.ProcessesInfo
+import nathan.Protocols.top.mac.{JpsItem, ProcessesInfo}
 
 import scala.sys.process._
 
@@ -17,13 +17,18 @@ object OS {
       case "Linux" => Linux
     }
   }
-  def top(os:OS=OSX) = os match {
+
+  def top(os: OS = OSX) = os match {
     case OSX => "top -l 1".!!.trim
     case Linux => "top -b -n 1".!!.trim
   }
-  def jps(os:OS=OSX)= os match {
-    case OSX => "jps".!!.trim
-    case Linux => "jps".!!.trim
+
+  def `jps -l`: List[JpsItem] = {
+    val Extract ="""(\d+)\s+(.*)\s*""".r
+    "jps -l".!!.trim.split("\n").map { line =>
+      val Extract(pid, name) = line
+      JpsItem(pid.toInt, name)
+    }.toList
   }
 }
 
