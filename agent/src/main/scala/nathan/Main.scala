ffd85f9081c613eb1d7b92326dd543d1c1bc5ae6
jianghang <hangscer@hotmail.com>
2018-01-28 11:12:32 +0800

    优化sigar jni库导入

--- agent/src/main/scala/nathan/Main.scala
+++ agent/src/main/scala/nathan/Main.scala
@@ -1,6 +1,6 @@
 package nathan
 
-import java.io.File
+import java.io.{File, FileOutputStream}
 import java.nio.file.Paths
 
 import akka.actor.{ActorSystem, Props}
@@ -12,16 +12,7 @@ import org.hyperic.sigar.Sigar
 
 object Main {
   def main(args: Array[String]): Unit = {
-    //    val system = ActorSystem(agent_system_name, ConfigFactory.load().withFallback(ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$agent_port")))
-    //    system.actorOf(Props(clazz = classOf[AgentActor]), agent_actor_name)
-    val sigar: Sigar = {
-      val a = this.getClass.getResource("/sigar").getPath
-      println(a)
-      val path = Paths.get(this.getClass.getResource("/sigar").getPath)
-      val sigarLibDir = path.resolve("sigar")
-      System.setProperty("java.library.path", System.getProperty("java.library.path") + (if (isWinos()) ";" else ":") + sigarLibDir.toString)
-      new Sigar()
-    }
-    println(sigar.getCpu)
+    val system = ActorSystem(agent_system_name, ConfigFactory.load().withFallback(ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$agent_port")))
+    system.actorOf(Props(clazz = classOf[AgentActor]), agent_actor_name)
   }
 }
