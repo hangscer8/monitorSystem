7ddb36fa21fa21105fe09834c0610d051fdae89f
jianghang <hangscer@gmail.com>
2018-02-01 19:42:25 +0800

    "哈哈😄"

--- centerRouter/src/main/scala/nathan/actor/CenterRouterActor.scala
+++ centerRouter/src/main/scala/nathan/actor/CenterRouterActor.scala
@@ -3,10 +3,10 @@ package nathan.actor
 import akka.actor.{Actor, Props}
 import nathan.actor.Protocol.StoreAgentMachineAction
 import nathan.ec.ExecutorService.daoActor
-import nathan.monitorSystem.AkkaSystemConst
+import nathan.monitorSystem.AkkaSystemConst._
 import nathan.monitorSystem.akkaAction.AgentActorJoinCenter
 
-class CenterRouterActor extends Actor with AkkaSystemConst{
+class CenterRouterActor extends Actor {
   override def receive: Receive = {
     case AgentActorJoinCenter(agentActor, agentMachineEntity) =>
       daoActor ! StoreAgentMachineAction(agentMachineEntity)
