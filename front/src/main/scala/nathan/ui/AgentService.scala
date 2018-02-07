281b7f14e2d47dd4fe4d776d85725d7612cde450
jianghang <hangscer@gmail.com>
2018-02-07 19:05:48 +0800

    binding.scala简单模块化

--- front/src/main/scala/nathan/ui/AgentService.scala
+++ front/src/main/scala/nathan/ui/AgentService.scala
@@ -1,11 +1,39 @@
 package nathan.ui
 
+import com.thoughtworks.binding.Binding.Var
 import com.thoughtworks.binding.{dom, _}
 import org.scalajs.dom.html._
+import nathan.util.implicitUtil._
+import org.scalajs.dom.raw.Event
 
 import scala.scalajs.js.annotation.JSExport
+import scala.util.Try
+
 @JSExport
 object AgentServiceUI {
+  var ip = "" //这里为什么不需要@dom
+  var port = 0
+
+  //使用@dom注解，使之模块化
+  @dom val ipInput: Binding[Input] = <input type="text"
+                                            id="agentIP"
+                                            onchange={e: Event =>
+                                              val target = e.currentTarget.as[Input]
+                                              ip = target.value}
+                                            class="form-control"
+                                            placeholder="input the ip address of agent machine"/>
+  @dom val portInput: Binding[Input] = <input type="number"
+                                              id="agentPort"
+                                              onchange={e: Event =>
+                                                val target = e.currentTarget.as[Input]
+                                                port = target.value.nonEmpty match {
+                                                  case true =>
+                                                    Try(target.value.toInt).getOrElse(0)
+                                                  case false => 0
+                                                }}
+                                              class="form-control"
+                                              placeholder="input the machine target port"/>
+
   @dom def addAgentDivComponent: Binding[Div] = { //添加一个Agent的Form组件
     <div class="row">
       <div class="col-md-4 col-md-offset-4 text-center text-info">Add Agent Machine</div>
@@ -14,24 +42,19 @@ object AgentServiceUI {
           <label for="agentIP" class="control-label ">
             <span class="glyphicon glyphicon-globe"/>
             Agent Ip address
-          </label>
-          <input type="text"
-                 id="agentIP"
-                 class="form-control"
-                 placeholder="input the ip address of agent machine"/>
+          </label>{ipInput.bind}
         </div>
         <div class="form-group">
           <label for="agentPort" class="control-label">
             <span class="glyphicon glyphicon-screenshot"/>
             Agent Port
-          </label>
-          <input type="number"
-                 id="agentPort"
-                 class="form-control"
-                 placeholder="input the machine target port"/>
+          </label>{portInput.bind}
         </div>
         <input type="button"
                value="连接"
+               onclick={e: Event =>
+                 println(ip)
+                 println(port)}
                class="col-md-2 col-md-offset-3 btn btn-primary"/>
         <input type="button" value="清除" class="col-md-2 col-md-offset-1 btn btn-warning"/>
       </form>
