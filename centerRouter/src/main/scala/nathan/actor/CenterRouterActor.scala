93b544145d02c775fcc40ba4d10b737eb7974787
hangscer <hangscer@hotmail.com>
2018-03-11 22:44:41 +0800

    优化日志

--- centerRouter/src/main/scala/nathan/actor/CenterRouterActor.scala
+++ centerRouter/src/main/scala/nathan/actor/CenterRouterActor.scala
@@ -1,18 +1,18 @@
 package nathan.actor
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorLogging, Props}
 import nathan.dbentity.EntityTable._
 import nathan.dbentity.EntityTable.h2.api._
 import nathan.monitorSystem.AkkaSystemConst._
 import nathan.util.FutureUtil.FutureOps
 import nathan.util.ImperativeRequestContext
 
-class CenterRouterActor extends Actor {
+class CenterRouterActor extends Actor with ActorLogging{
   override def receive: Receive = {
     case (ctx: ImperativeRequestContext, ip: String, port: Int) =>
       context.actorOf(Props(classOf[PeerToAgentActor])) ! (ctx, ip, port) //转发
     case (`agentTimeout`, agentId: String) =>
       db.run(agentMachines.filter(_.agentId === agentId).delete.asTry).exec
-      println(agentId + "退出了")
+      log.warning(s"agentId为${agentId}离线了")
   }
 }
